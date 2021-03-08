30acbe7d64a6d8081ae49667631a34321269ded2
gean-xyz <me@gean.at>
2021-03-08 19:07:01 +0100

    asd

--- vs/PingMe/pingme.cs
+++ vs/PingMe/pingme.cs
@@ -30,6 +30,23 @@ namespace PingMe
         process.StartInfo.UseShellExecute = false;
         process.StartInfo.RedirectStandardOutput = true;
         process.Start();
+        Console.WriteLine("found: " + process.StandardOutput);
+
+        var pingproc = new Process();
+        try
+        {
+          pingproc.StartInfo.FileName = process.StandardOutput.ToString();
+          pingproc.StartInfo.Arguments = "-c 2 localhost";
+          pingproc.StartInfo.CreateNoWindow = false;
+          pingproc.StartInfo.UseShellExecute = false;
+          pingproc.StartInfo.RedirectStandardOutput = true;
+
+          Console.WriteLine("pingproc stdout: " + pingproc.StandardOutput);
+          Console.WriteLine("pingproc stderr: " + pingproc.StandardError);
+
+        } catch(Exception e) {
+          Console.WriteLine("error on pingproc: " + e);
+        }
       }
       catch (Exception e)
       {
